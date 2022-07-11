21fe65b10b06bd56d761dba90dadf7147b15d054
Radar3759 <bunnybridgemitten@gmail.com>
2022-07-11 16:57:21 -0700

    -update app.py and user.py

--- Week 3/app.py
+++ Week 3/app.py
@@ -1,9 +1,9 @@
 #up to task 3 works
 '''useful docstring'''
 from donation_pkg import homepage
-from donation_pkg import user
+from donation_pkg.user import *
 #declare a var named database
-#assign key:value pair 
+#assign key:value pair
 database = {
     'admin': 'admin',
     'password': 'password123'
@@ -15,16 +15,18 @@ homepage.show_homepage()
 if authorized_user == " ":
     print("You must be logged in to donate.")
 else:
-    print(f"Logged in as:", {authorized_user})
+    print("Logged in as:", authorized_user)
 
 option = input("Choose an option 1-5: ")
 
 while True:
     if option == "1":
-        print("TODO: Write Login Functionality")
-    elif option == "2":    
+        username = input("\nEnter your username: ")
+        password = input("\nEnter your password: ")
+        authorized_user = login(database, username, password)
+    elif option == "2":
         print("TODO: Write Register Functionality")
-        
+
     elif option == "3":
         print("TODO: Write Donate Functionality")
         # option = input("Choose an option 1-5: ")
