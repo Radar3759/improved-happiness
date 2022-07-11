426b6a1046407454f2276591d12fe9b8fab9bf0f
Radar3759 <bunnybridgemitten@gmail.com>
2022-07-11 16:40:14 -0700

    -update app.py

--- Week 3/app.py
+++ Week 3/app.py
@@ -1,41 +1,39 @@
 #up to task 3 works
+'''useful docstring'''
 from donation_pkg import homepage
 from donation_pkg import user
-
+#declare a var named database
+#assign key:value pair 
 database = {
     'admin': 'admin',
     'password': 'password123'
 }
 donations = {}
-authorized_user = ""
+authorized_user = " "
 homepage.show_homepage()
 
-if authorized_user == "":
+if authorized_user == " ":
     print("You must be logged in to donate.")
 else:
-    print("Logged in as:", authorized_user)  
+    print(f"Logged in as:", {authorized_user})
 
 option = input("Choose an option 1-5: ")
 
-while True:  
-    
+while True:
     if option == "1":
         print("TODO: Write Login Functionality")
-        # username = input("\nEnter Username: ")
-        # password = input("Enter Password: ")
-        # authorized_user = (database, username, password)
-    elif option == "2": 
-        # username = input("\nEnter Username: ")
-        # password = input("Enter Password")
-        
+    elif option == "2":    
         print("TODO: Write Register Functionality")
-    elif option == "3": 
-        print("TODO: Write Donate Functionality") 
-    elif option == "4": 
-        print("TODO: Write Show Donations Functionality") 
+        
+    elif option == "3":
+        print("TODO: Write Donate Functionality")
+        # option = input("Choose an option 1-5: ")
+    elif option == "4":
+        print("TODO: Write Show Donations Functionality")
+        # option = input("Choose an option 1-5: ")
     elif option == "5":
         print("Goodbye!")
         break
-#test Task 3 
-    #option = input("Pick again: ")
-    break
+#test Task 3
+    option = input("Pick again: ")
+
